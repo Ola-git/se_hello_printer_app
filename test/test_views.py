eb184520baafcac1d9b7f2c9c743070e3ebed063
vdi-student <olapyt@users.noreply.github.com>
2020-05-23 11:10:47 +0200

    fix test after change name

--- test/test_views.py
+++ test/test_views.py
@@ -15,7 +15,7 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEqual(b'{ "imie":"Ola", "mgs":"Hello World!"}', rv.data)
+        self.assertEqual(b'{ "imie":"Olenka", "mgs":"Hello World!"}', rv.data)
 
     def test_msg_with_output_xml(self):
         rv = self.app.get('/?output=xml')
